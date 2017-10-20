d136709307e8691e9ac1e295c82305825e85a889
xiong Mingcai <xmc000@icloud.com>
2017-10-20 20:58:43 +0800

    'add '

---
+++ src/layout.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <wd-header></wd-header>
+            <router-view>
+                <wd-bout></wd-bout>
+                <wd-index></wd-index>
+                <wd-bout></wd-bout>
+            </router-view>
+        <wd-footer></wd-footer>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import wdHeader from 'src/components/wd-header.vue'
+    import wdFooter from 'src/components/wd-footer.vue'
+
+    import wdIndex from 'src/page/index.vue'
+    import wdBout from 'src/page/about.vue'
+    import wdLianxi from 'src/page/lianxi.vue'
+    import wdZixun from 'src/page/zixun.vue'
+
+    export default{
+        components: {
+            wdHeader,
+            wdFooter,
+            wdIndex,
+            wdBout,
+            wdLianxi,
+            wdZixun
+        },
+
+    }
+</script>
+
+<style lang="less" type="text/less" rel="stylesheet/less">
+    /*@import '../../assets/css/variable.less';*/
+</style>
---
+++ src/page/lianxi.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class='lianxi'>
+        <h1>lianxi</h1>
+        <h1>lianxi</h1>
+        <h1>lianxi</h1>
+        <h1>lianxi</h1>
+        <h1>lianxi</h1>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    // import logHeader from './log-header.vue'
+    export default{
+        data() {
+            return {}
+        },
+        components: {},
+        computed: {},
+        methods: {},
+        watch: {},
+        mounted(){
+
+        }
+    }
+</script>
+
+<style lang="less"  type="text/less" rel="stylesheet/less">
+    /*@import '../../assets/css/variable.less';*/
+
+</style>
